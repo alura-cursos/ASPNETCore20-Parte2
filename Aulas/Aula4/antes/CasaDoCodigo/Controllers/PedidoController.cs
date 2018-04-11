feb7992b1ab9ea81c1382f7a342037d4ee430d43
marcelooliveira <mclricardo@gmail.com>
2018-04-11 20:11:48 -0300

    Aula4 - antes

---
+++ Aulas/Aula4/antes/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,60 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
+using CasaDoCodigo.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository pedidoRepository;
+        private readonly IItemPedidoRepository itemPedidoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository,
+            IPedidoRepository pedidoRepository,
+            IItemPedidoRepository itemPedidoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+            this.pedidoRepository = pedidoRepository;
+            this.itemPedidoRepository = itemPedidoRepository;
+        }
+
+        public IActionResult Carrossel()
+        {
+            return View(produtoRepository.GetProdutos());
+        }
+
+        public IActionResult Carrinho(string codigo)
+        {
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                pedidoRepository.AddItem(codigo);
+            }
+
+            List<ItemPedido> itens = pedidoRepository.GetPedido().Itens;
+            CarrinhoViewModel carrinhoViewModel = new CarrinhoViewModel(itens);
+            return base.View(carrinhoViewModel);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+
+        public IActionResult Resumo()
+        {
+            return View(pedidoRepository.GetPedido());
+        }
+
+        [HttpPost]
+        public UpdateQuantidadeResponse UpdateQuantidade([FromBody]ItemPedido itemPedido)
+        {
+            return pedidoRepository.UpdateQuantidade(itemPedido);
+        }
+    }
+}
---
+++ Aulas/Aula4/depois/CasaDoCodigo/DataService.cs
@@ -0,0 +1,40 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto,
+            IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            contexto.Database.Migrate();
+
+            List<Livro> livros = GetLivros();
+
+            produtoRepository.SaveProdutos(livros);
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+
+}
